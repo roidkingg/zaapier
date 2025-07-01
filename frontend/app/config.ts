08230fcb01e4ccecc702beb30d6991dd7a565d90
Aryan <rarharyan@gmail.com>
2025-07-02 00:52:58 +0530

    efwr

--- frontend/app/config.ts
+++ frontend/app/config.ts
@@ -1,3 +1,3 @@
 
-export const BACKEND_URL = "http://localhost:4000"
-export const HOOKS_URL = "http://localhost:3002"
+export const BACKEND_URL = "http://13.217.167.70:4000/"
+export const HOOKS_URL = "http://13.217.167.70:3002/"
