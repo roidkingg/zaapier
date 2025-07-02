e90af8a32041564739b3db943e372c8c2a12dd25
Aryan <rarharyan@gmail.com>
2025-07-02 20:19:58 +0530

    backend and hooks set to https

--- frontend/app/config.ts
+++ frontend/app/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://13.217.167.70:4000"
-export const HOOKS_URL = "http://13.217.167.70:3002"
+export const BACKEND_URL = "https://api.aryanrarh.dev"
+export const HOOKS_URL = "https://hooks.aryanrarh.dev"
